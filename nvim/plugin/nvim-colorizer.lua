5e82c73adcb292b0013506ee05be6139df02e98c
koen-benne <koen.benne@gmail.com>
2024-06-18 17:34:13 +0200

    add colorizer config

---
+++ nvim/plugin/nvim-colorizer.lua
@@ -0,0 +1,17 @@
+if vim.g.did_load_nvim_colorizer_plugin then
+  return
+end
+vim.g.did_load_nvim_colorizer_plugin = true
+
+local status_ok, colorizer = pcall(require, "colorizer")
+if not status_ok then
+  return
+end
+
+colorizer.setup {
+  '*';
+  conf = { RRGGBB = true };
+  css = { rgb_fn = true };
+  html = { names = false };
+  rasi = { RRGGBBAA = true };
+}
