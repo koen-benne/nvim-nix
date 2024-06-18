bf076aba299928fb2db2035e7295d33c23c3c05e
koen-benne <koen.benne@gmail.com>
2024-06-18 16:53:00 +0200

    update lock

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
     #   url = "github:Cassin01/wf.nvim";
     #   flake = false;
     # };
+    project-nvim = {
+      url = "github:ahmedkhalf/project.nvim";
+      flake = false;
+    };
   };
 
   outputs = inputs @ {
