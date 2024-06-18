e8f04671afcabad8437eb08b3ca836c3c80b2843
koen-benne <koen.benne@gmail.com>
2024-06-18 17:30:00 +0200

    add copilot config

---
+++ nvim/plugin/copilot.lua
@@ -0,0 +1,23 @@
+if vim.g.did_load_copilot_plugin then
+  return
+end
+vim.g.did_load_copilot_plugin = true
+
+-- vim.cmd [[
+--   imap <silent><script><expr> <C-A> copilot#Accept("\<CR>")
+--   let g:copilot_no_tab_map = v:true
+-- ]]
+
+local status_ok, copilot = pcall(require, "copilot")
+if not status_ok then
+  return
+end
+
+local status_comment_ok, comment = pcall(require, "comment")
+
+copilot.setup {
+  copilot_node_command = vim.fn.expand("$HOME") .. "/.nix-profile/bin/node",
+  suggestion = {
+    auto_trigger = true,
+  },
+}
