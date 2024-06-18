ad8f2d64f0ebc9b20006b074d923c62d0b1dca3e
koen-benne <koen.benne@gmail.com>
2024-06-18 17:22:40 +0200

    update keymaps

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -3,198 +3,144 @@ if vim.g.did_load_keymaps_plugin then
 end
 vim.g.did_load_keymaps_plugin = true
 
-local api = vim.api
-local fn = vim.fn
-local keymap = vim.keymap
-local diagnostic = vim.diagnostic
+local setKeymap = vim.keymap.set
 
--- Yank from current position till end of current line
-keymap.set('n', 'Y', 'y$', { silent = true, desc = '[Y]ank to end of line' })
+-- Make space leader key
+setKeymap("", "<Space>", "<Nop>")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- Buffer list navigation
-keymap.set('n', '[b', vim.cmd.bprevious, { silent = true, desc = 'previous [b]uffer' })
-keymap.set('n', ']b', vim.cmd.bnext, { silent = true, desc = 'next [b]uffer' })
-keymap.set('n', '[B', vim.cmd.bfirst, { silent = true, desc = 'first [B]uffer' })
-keymap.set('n', ']B', vim.cmd.blast, { silent = true, desc = 'last [B]uffer' })
+setKeymap("n", "<C-e>", "<Nop>")
 
--- Toggle the quickfix list (only opens if it is populated)
-local function toggle_qf_list()
-  local qf_exists = false
-  for _, win in pairs(fn.getwininfo() or {}) do
-    if win['quickfix'] == 1 then
-      qf_exists = true
-    end
-  end
-  if qf_exists == true then
-    vim.cmd.cclose()
-    return
-  end
-  if not vim.tbl_isempty(vim.fn.getqflist()) then
-    vim.cmd.copen()
-  end
-end
+-- Normal mode --
+-- Window nav
+setKeymap("n", "<C-h>", "<C-w>h")
+setKeymap("n", "<C-j>", "<C-w>j")
+setKeymap("n", "<C-k>", "<C-w>k")
+setKeymap("n", "<C-l>", "<C-w>l")
 
-keymap.set('n', '<C-c>', toggle_qf_list, { desc = 'toggle quickfix list' })
+-- Resize with arrows
+setKeymap("n", "<C-[>", ":vertical resize -2<CR>")
+setKeymap("n", "<C-]>", ":vertical resize +2<CR>")
 
-local function try_fallback_notify(opts)
-  local success, _ = pcall(opts.try)
-  if success then
-    return
-  end
-  success, _ = pcall(opts.fallback)
-  if success then
-    return
-  end
-  vim.notify(opts.notify, vim.log.levels.INFO)
-end
+-- Naviagate buffers
+setKeymap("n", "<S-l>", ":bnext<CR>")
+setKeymap("n", "<S-h>", ":bprevious<CR>")
 
--- Cycle the quickfix and location lists
-local function cleft()
-  try_fallback_notify {
-    try = vim.cmd.cprev,
-    fallback = vim.cmd.clast,
-    notify = 'Quickfix list is empty!',
-  }
-end
+-- Move text up and down
+setKeymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi")
+setKeymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi")
 
-local function cright()
-  try_fallback_notify {
-    try = vim.cmd.cnext,
-    fallback = vim.cmd.cfirst,
-    notify = 'Quickfix list is empty!',
-  }
-end
-
-keymap.set('n', '[c', cleft, { silent = true, desc = '[c]ycle quickfix left' })
-keymap.set('n', ']c', cright, { silent = true, desc = '[c]ycle quickfix right' })
-keymap.set('n', '[C', vim.cmd.cfirst, { silent = true, desc = 'first quickfix entry' })
-keymap.set('n', ']C', vim.cmd.clast, { silent = true, desc = 'last quickfix entry' })
-
-local function lleft()
-  try_fallback_notify {
-    try = vim.cmd.lprev,
-    fallback = vim.cmd.llast,
-    notify = 'Location list is empty!',
-  }
-end
-
-local function lright()
-  try_fallback_notify {
-    try = vim.cmd.lnext,
-    fallback = vim.cmd.lfirst,
-    notify = 'Location list is empty!',
-  }
-end
-
-keymap.set('n', '[l', lleft, { silent = true, desc = 'cycle [l]oclist left' })
-keymap.set('n', ']l', lright, { silent = true, desc = 'cycle [l]oclist right' })
-keymap.set('n', '[L', vim.cmd.lfirst, { silent = true, desc = 'first [L]oclist entry' })
-keymap.set('n', ']L', vim.cmd.llast, { silent = true, desc = 'last [L]oclist entry' })
-
--- Resize vertical splits
-local toIntegral = math.ceil
-keymap.set('n', '<leader>w+', function()
-  local curWinWidth = api.nvim_win_get_width(0)
-  api.nvim_win_set_width(0, toIntegral(curWinWidth * 3 / 2))
-end, { silent = true, desc = 'inc window [w]idth' })
-keymap.set('n', '<leader>w-', function()
-  local curWinWidth = api.nvim_win_get_width(0)
-  api.nvim_win_set_width(0, toIntegral(curWinWidth * 2 / 3))
-end, { silent = true, desc = 'dec window [w]idth' })
-keymap.set('n', '<leader>h+', function()
-  local curWinHeight = api.nvim_win_get_height(0)
-  api.nvim_win_set_height(0, toIntegral(curWinHeight * 3 / 2))
-end, { silent = true, desc = 'inc window [h]eight' })
-keymap.set('n', '<leader>h-', function()
-  local curWinHeight = api.nvim_win_get_height(0)
-  api.nvim_win_set_height(0, toIntegral(curWinHeight * 2 / 3))
-end, { silent = true, desc = 'dec window [h]eight' })
-
--- Close floating windows [Neovim 0.10 and above]
-keymap.set('n', '<leader>fq', function()
-  vim.cmd('fclose!')
-end, { silent = true, desc = '[f]loating windows: [q]uit/close all' })
-
--- Remap Esc to switch to normal mode and Ctrl-Esc to pass Esc to terminal
-keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'switch to normal mode' })
-keymap.set('t', '<C-Esc>', '<Esc>', { desc = 'send Esc to terminal' })
-
--- Shortcut for expanding to current buffer's directory in command mode
-keymap.set('c', '%%', function()
-  if fn.getcmdtype() == ':' then
-    return fn.expand('%:h') .. '/'
+-- Switch between tab sizes 2 and 4
+setKeymap("n", "<leader>t", function()
+  if(vim.opt.shiftwidth:get() == 2) then
+    vim.opt.shiftwidth = 4
+    vim.opt.tabstop= 4
   else
-    return '%%'
+    vim.opt.shiftwidth = 2
+    vim.opt.tabstop= 2
   end
-end, { expr = true, desc = "expand to current buffer's directory" })
+end)
 
-keymap.set('n', '<space>tn', vim.cmd.tabnew, { desc = '[t]ab: [n]ew' })
-keymap.set('n', '<space>tq', vim.cmd.tabclose, { desc = '[t]ab: [q]uit/close' })
+-- Insert --
+-- Press jk fast to enter
+setKeymap("i", "jk", "<ESC>")
 
-local severity = diagnostic.severity
+-- Visual --
+-- Stay in indent mode
+setKeymap("v", "<", "<gv")
+setKeymap("v", ">", ">gv")
 
-keymap.set('n', '<space>e', function()
-  local _, winid = diagnostic.open_float(nil, { scope = 'line' })
+-- Move text up and down
+setKeymap("v", "<A-j>", ":m .+1<CR>==")
+setKeymap("v", "<A-k>", ":m .-2<CR>==")
+setKeymap("v", "p", '"_dP')
+
+-- Visual Block --
+-- Move text up and down
+setKeymap("x", "J", ":move '>+1<CR>gv-gv")
+setKeymap("x", "K", ":move '<-2<CR>gv-gv")
+setKeymap("x", "<A-j>", ":move '>+1<CR>gv-gv")
+setKeymap("x", "<A-k>", ":move '<-2<CR>gv-gv")
+
+-- Custom --
+-- Telescope --
+setKeymap("", "<Leader>pp", ":lua require'telescope.builtin'.builtin{}<CR>")
+-- most recently used files
+setKeymap("", "<Leader>o", ":lua require'telescope.builtin'.oldfiles{}<CR>")
+-- find buffer
+setKeymap("", ";", ":lua require'telescope.builtin'.buffers{}<CR>")
+-- find in current buffer
+setKeymap("", "<Leader>/", ":lua require'telescope.builtin'.current_buffer_fuzzy_find{}<CR>")
+-- bookmarks
+setKeymap("", "<Leader>'", ":lua require'telescope.builtin'.marks{}<CR>")
+-- git files
+setKeymap("", "<Leader>gi", ":lua require'telescope.builtin'.git_files{}<CR>")
+-- all files
+setKeymap("", "<Leader>sf", ":lua require'telescope.builtin'.find_files{}<CR>")
+-- ripgrep like grep through dir
+setKeymap("", "<Leader>rg", ":lua require'telescope.builtin'.live_grep{}<CR>")
+-- ripgrep but with hidden files
+setKeymap("", "<Leader>ga", ":lua require'telescope.builtin'.live_grep{ vimgrep_arguments = { 'rg', '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-u', '--hidden' } }<CR>")
+-- pick color scheme
+setKeymap("", "<Leader>cs", ":lua require'telescope.builtin'.colorscheme{}<CR>")
+-- undo history
+setKeymap("n", "<Leader>u", ":Telescope undo<CR>")
+-- Harpoon keymaps
+setKeymap("n", "<leader>mk", ":lua require'harpoon.mark'.add_file()<CR>")
+--[[ setKeymap("n", "<leader>hr", ":lua require'harpoon.ui'.toggle_quick_menu()<CR>") ]]
+setKeymap("n", "<leader>hr", ":Telescope harpoon marks<CR>")
+
+-- NVIM Tree --
+--[[ setKeymap("n", "<C-t>", ":NvimTreeToggle<CR>") ]]
+
+-- UFO --
+setKeymap("n", "zP", function()
+  local winid = require("ufo").peekFoldedLinesUnderCursor()
   if not winid then
-    vim.notify('no diagnostics found', vim.log.levels.INFO)
-    return
+    vim.lsp.buf.hover()
   end
-  vim.api.nvim_win_set_config(winid or 0, { focusable = true })
-end, { noremap = true, silent = true, desc = 'diagnostics floating window' })
-keymap.set('n', '[d', diagnostic.goto_prev, { noremap = true, silent = true, desc = 'previous [d]iagnostic' })
-keymap.set('n', ']d', diagnostic.goto_next, { noremap = true, silent = true, desc = 'next [d]iagnostic' })
-keymap.set('n', '[e', function()
-  diagnostic.goto_prev {
-    severity = severity.ERROR,
-  }
-end, { noremap = true, silent = true, desc = 'previous [e]rror diagnostic' })
-keymap.set('n', ']e', function()
-  diagnostic.goto_next {
-    severity = severity.ERROR,
-  }
-end, { noremap = true, silent = true, desc = 'next [e]rror diagnostic' })
-keymap.set('n', '[w', function()
-  diagnostic.goto_prev {
-    severity = severity.WARN,
-  }
-end, { noremap = true, silent = true, desc = 'previous [w]arning diagnostic' })
-keymap.set('n', ']w', function()
-  diagnostic.goto_next {
-    severity = severity.WARN,
-  }
-end, { noremap = true, silent = true, desc = 'next [w]arning diagnostic' })
-keymap.set('n', '[h', function()
-  diagnostic.goto_prev {
-    severity = severity.HINT,
-  }
-end, { noremap = true, silent = true, desc = 'previous [h]int diagnostic' })
-keymap.set('n', ']h', function()
-  diagnostic.goto_next {
-    severity = severity.HINT,
-  }
-end, { noremap = true, silent = true, desc = 'next [h]int diagnostic' })
+end)
 
-local function toggle_spell_check()
-  ---@diagnostic disable-next-line: param-type-mismatch
-  vim.opt.spell = not (vim.opt.spell:get())
+-- Debugging --
+setKeymap("n", "<F5>", ":lua require'dap'.continue()<CR>")
+setKeymap("n", "<F10>", ":lua require'dap'.step_over()<CR>")
+setKeymap("n", "<F11>", ":lua require'dap'.step_into()<CR>")
+setKeymap("n", "<F12>", ":lua require'dap'.step_out()<CR>")
+setKeymap("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
+setKeymap("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+setKeymap("n", "<leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
+setKeymap("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+  require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+  require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end)
+setKeymap("n", "<leader>do", ":lua require'dapui'.toggle()<CR>")
+
+-- Gitsigns --
+setKeymap("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
+
+-- Terminal --
+-- TODO: change exit insert mode keymap when in this window (also nice for terminal)
+setKeymap("n", "<leader>lg", ":lua _LAZYGIT_TOGGLE()<CR>")
+setKeymap("n", "<leader>gu", ":lua _GITUI_TOGGLE()<CR>")
+-- setKeymap("n", "<leader>bc", ":lua _BACON_TOGGLE()<CR>")
+
+-- Neovide keymaps
+if vim.g.neovide then
+  setKeymap("n", "+", ":lua ChangeScaleFactor(1.25)<CR>")
+  setKeymap("n", "_", ":lua ChangeScaleFactor(1/1.25)<CR>")
 end
 
-keymap.set('n', '<leader>S', toggle_spell_check, { noremap = true, silent = true, desc = 'toggle [S]pell' })
 
-keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'move [d]own half-page and center' })
-keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'move [u]p half-page and center' })
-keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'move DOWN [f]ull-page and center' })
-keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'move UP full-page and center' })
-
---- Disabled keymaps [enable at your own risk]
-
--- Automatic management of search highlight
--- XXX: This is not so nice if you use j/k for navigation
--- (you should be using <C-d>/<C-u> and relative line numbers instead ;)
---
--- local auto_hlsearch_namespace = vim.api.nvim_create_namespace('auto_hlsearch')
--- vim.on_key(function(char)
---   if vim.fn.mode() == 'n' then
---     vim.opt.hlsearch = vim.tbl_contains({ '<CR>', 'n', 'N', '*', '#', '?', '/' }, vim.fn.keytrans(char))
---   end
--- end, auto_hlsearch_namespace)
