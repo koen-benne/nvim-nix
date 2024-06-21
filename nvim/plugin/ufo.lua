f9a3a6ba7d4a9f95e2337f4eebb07f1f707b0809
koen-benne <koen.benne@gmail.com>
2024-06-21 14:38:39 +0200

    update

--- nvim/plugin/ufo.lua
+++ nvim/plugin/ufo.lua
@@ -13,8 +13,3 @@ ufo.setup({
     return {'lsp', 'indent'}
   end,
 })
-
--- buffer scope handler
--- will override global handler if it is existed
-local bufnr = vim.api.nvim_get_current_buf()
-require('ufo').setFoldVirtTextHandler(bufnr, handler)
