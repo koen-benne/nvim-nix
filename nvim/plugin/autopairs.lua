567b892b3db8114a22b35f508cfb44477a7d9f98
koen-benne <koen.benne@gmail.com>
2024-06-24 11:15:21 +0200

    disable autopairs

--- nvim/plugin/autopairs.lua
+++ nvim/plugin/autopairs.lua
@@ -1,7 +1,7 @@
+vim.g.did_load_autopairs_plugin = true
 if vim.g.did_load_autopairs_plugin then
   return
 end
-vim.g.did_load_autopairs_plugin = true
 
 -- Setup autopairs and autotags.
 local status_ok, npairs = pcall(require, "nvim-autopairs")
