565e74463880bb68efbed2420290f56b4fe4be36
koen-benne <koen.benne@gmail.com>
2024-06-25 10:43:39 +0200

    update

--- nvim/lua/user/lazy/init.lua
+++ nvim/lua/user/lazy/init.lua
@@ -2,6 +2,7 @@
 require('user.lazy.cmp')
 require('user.lazy.lazy-lsp')
 require('user.lazy.copilot')
-require('user.lazy.nvim-dap')
 require('user.lazy.neo-tree')
+-- Telescope before dap, because dap config depends on telescope
 require('user.lazy.telescope')
+require('user.lazy.nvim-dap')
