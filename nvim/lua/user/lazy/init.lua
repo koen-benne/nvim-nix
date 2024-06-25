48346ec7ee4a149e8aded91bf1d1f97b2a24bdc3
koen-benne <koen.benne@gmail.com>
2024-06-25 10:16:11 +0200

    cmp before lsp

--- nvim/lua/user/lazy/init.lua
+++ nvim/lua/user/lazy/init.lua
@@ -1,4 +1,5 @@
-require('user.lazy.lazy-lsp')
+-- CMP before lsp, because lsp config depends on cmp stuff
 require('user.lazy.cmp')
+require('user.lazy.lazy-lsp')
 require('user.lazy.copilot')
 require('user.lazy.nvim-dap')
