e760696a8a13ea5b4079c30419c552c2e9f81d92
koen-benne <koen.benne@gmail.com>
2024-06-25 18:42:26 +0200

    update order of lazyloaded plugins

--- nvim/lua/user/lazy/init.lua
+++ nvim/lua/user/lazy/init.lua
@@ -1,14 +1,14 @@
--- CMP before lsp, because lsp config depends on cmp stuff
-require('user.lazy.cmp')
-require('user.lazy.lazy-lsp')
-require('user.lazy.copilot')
+require('user.lazy.lualine')
 require('user.lazy.neo-tree')
 -- Harpoon before telescope, because telescope config depends on harpoon
 require('user.lazy.harpoon')
 -- Telescope before dap, because dap config depends on telescope
 require('user.lazy.telescope')
+-- CMP before lsp, because lsp config depends on cmp stuff
+require('user.lazy.cmp')
+require('user.lazy.lazy-lsp')
+require('user.lazy.copilot')
 require('user.lazy.nvim-dap')
-require('user.lazy.lualine')
 require('user.lazy.toggleterm')
 require('user.lazy.nvim-colorizer')
 require('user.lazy.illuminate')
