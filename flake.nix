a9ddd6896985a6d97c3f9982abe180929488af2e
Koen Benne <koen.benne@gmail.com>
2024-07-29 10:10:34 +0200

    add check
    s

--- flake.nix
+++ flake.nix
@@ -40,6 +40,16 @@
       "aarch64-darwin"
     ];
 
+    forEachDir = exec: ''
+      for dir in */; do
+        (
+          cd "''${dir}"
+
+          ${exec}
+        )
+      done
+    '';
+
     # This is where the Neovim derivation is built.
     neovim-overlay = import ./nix/neovim-overlay.nix {inherit inputs;};
   in
@@ -64,6 +74,13 @@
           nil
           stylua
           luajitPackages.luacheck
+          (writeShellApplication {
+            name = "check";
+            text = forEachDir ''
+              echo "checking ''${dir}"
+              nix flake check --all-systems --no-build
+            '';
+          })
         ];
         shellHook = ''
           # symlink the .luarc.json generated in the overlay
