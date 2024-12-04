2eeb2f8a1b5f5f72914ad1e270c7dd3e02458ea3
Koen Benne <koen.benne@gmail.com>
2024-12-04 13:32:43 +0100

    fix ufo

--- nvim/lua/user/lazy/ufo.lua
+++ nvim/lua/user/lazy/ufo.lua
@@ -3,7 +3,7 @@ if vim.g.did_load_ufo_plugin then
 end
 vim.g.did_load_ufo_plugin = true
 
-vim.cmd('packadd ufo-nvim')
+vim.cmd('packadd nvim-ufo')
 
 local status, ufo = pcall(require, 'ufo')
 if not status then
