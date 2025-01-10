2d22614f41e8d1ab6ea216321218eff18c23c3c7
Koen Benne <koen.benne@gmail.com>
2025-01-10 16:12:38 +0100

    remove autopais cmp stuff

--- nvim/plugin/autopairs.lua
+++ nvim/plugin/autopairs.lua
@@ -35,9 +35,3 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagn
   update_in_insert = true,
 })
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp_status_ok, cmp = pcall(require, 'cmp')
-if not cmp_status_ok then
-  return
-end
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
