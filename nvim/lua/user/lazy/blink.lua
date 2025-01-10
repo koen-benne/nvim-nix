f467c3102f70766866917f23be56bd10d9d05152
Koen Benne <koen.benne@gmail.com>
2025-01-10 16:02:30 +0100

    set up blink

--- nvim/lua/user/lazy/blink.lua
+++ nvim/lua/user/lazy/blink.lua
@@ -17,33 +17,32 @@ if not blink_status_ok then
   vim.notify('Blink is not installed!')
   return
 end
-vim.notify('Blink is not installed!')
-
-local snip_status_ok, luasnip = pcall(require, 'luasnip')
-if not snip_status_ok then
-  vim.notify('Luasnip is not installed!')
-  return
-end
-
-local buffer_fts = {
-  'markdown',
-  'toml',
-  'yaml',
-  'json',
-}
-
-require('luasnip/loaders/from_vscode').lazy_load()
-
-local check_backspace = function()
-  local col = vim.fn.col('.') - 1
-  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
-end
-
-local icons = require('user.icons')
-
-local kind_icons = icons.kind
 
 vim.api.nvim_set_hl(0, 'CmpItemKindTabnine', { fg = '#CA42F0' })
 vim.api.nvim_set_hl(0, 'CmpItemKindEmoji', { fg = '#FDE030' })
 vim.api.nvim_set_hl(0, 'CmpItemKindCrate', { fg = '#F64D00' })
 
+blink.setup({
+  enabled = function()
+    return not vim.tbl_contains({ "lua", "markdown" }, vim.bo.filetype)
+      and vim.bo.buftype ~= 'prompt'
+      and vim.bo.completion ~= false
+    end,
+  completion = {
+    keyword = { range = 'full' },
+    tigger = { show_on_trigge_characte = true },
+    list = { selection = { peselect = false, auto_insert = true } },
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+  },
+
+  keymap = {
+    preset = 'super-tab',
+  },
+
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
+  },
+
+  snippets = { preset = 'luasnip' },
+  signatue = { enabled = true },
+})
